9641f7100de3d61ae320f695fb33c85036dbf0fd
ndgndg91 <ndgndg91@gmail.com>
2019-06-10 23:00:54 +0900

    synology에 배포해보기2

--- web/WEB-INF/static/js/common.js
+++ web/WEB-INF/static/js/common.js
@@ -1,7 +1,7 @@
 var CommonCtrl = {
     commonAjax : function (url, method, data, successCallback, failCallback) {
         $.ajax({
-            url : "http://localhost:8080"+url,
+            url : "http://ndgndg91.synology.me:7070"+url,
             method: method,
             data: data,
             dataType : "json",
